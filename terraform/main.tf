11745dfce95a04c80b8cc2ef4c1b7fdd48ada0a4
oleh_laptop <leon6871@gmail.com>
2025-03-15 13:24:12 -0500

    workspace update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,23 @@
-terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "2Ops_test"
-    # workspaces {
-    #   name = "2Ops_qa"
-    # }
-  }
+# terraform {
+#   backend "remote" {
+#     hostname     = "app.terraform.io"
+#     organization = "2Ops_test"
+
+#     workspaces {
+#       name = "2Ops_qa"
+#     }
+#   }
+# }
+
+terraform { 
+  cloud { 
+    
+    organization = "2Ops_test" 
+
+    workspaces { 
+      name = "2Ops_dev" 
+    } 
+  } 
 }
 
 
